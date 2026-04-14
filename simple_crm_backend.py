0d61f7c426320dcd801b8bb68f2f9651f07e8078
ashishmehra69-netizen <ashishmehra69@gmail.com>
2026-04-14 12:12:47 +0530

    Update simple_crm_backend.py

--- simple_crm_backend.py
+++ simple_crm_backend.py
@@ -72,7 +72,7 @@ def add_cors_headers(resp):
 
 
 def get_conn():
-    return psycopg2.connect(DATABASE_URL, sslmode="prefer")
+    return psycopg2.connect(DATABASE_URL + "?sslmode=require")
 
 
 def init_db() -> None:
